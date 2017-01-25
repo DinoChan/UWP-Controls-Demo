9b9ad83bf606f130f8a037f467b3f037d3385b12
Dino.Chan <dino.chan@live.com>
2017-01-25 08:46:21 +0800

    Add Image Page

--- UwpControlsDemo/UwpControlsDemo/Samples/Image/ImagePage.xaml.cs
+++ UwpControlsDemo/UwpControlsDemo/Samples/Image/ImagePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
@@ -25,6 +27,35 @@ namespace UwpControlsDemo
         public ImagePage()
         {
             this.InitializeComponent();
+            
+            Actions = new ObservableCollection<string>();
+            Stretches = new List<Stretch> { Stretch.Fill, Stretch.None, Stretch.Uniform, Stretch.UniformToFill };
+        }
+
+        public ObservableCollection<string> Actions { get; }
+
+        public IEnumerable<Stretch> Stretches { get; }
+
+
+        private void OnImageLoaded(object sender, RoutedEventArgs e)
+        {
+            AddAction("ImageLoaded");
+        }
+
+        private void OnImageOpened(object sender, RoutedEventArgs e)
+        {
+            AddAction("ImageOpened");
+        }
+
+        private void OnImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            AddAction("ImageFailed");
+        }
+
+        private void AddAction(string action)
+        {
+            var content = DateTime.Now.ToString("HH:mm:ss") + " " + action;
+            Actions.Add(content);
         }
     }
 }
