883b97cba858c07d2b5bd248b3e647486606f027
dino <dino.chan@live.com>
2017-01-28 18:03:42 +0800

    Add EventListView

--- UwpControlsDemo/UwpControlsDemo/Controls/EventListView.cs
+++ UwpControlsDemo/UwpControlsDemo/Controls/EventListView.cs
@@ -7,11 +7,42 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Shapes;
 
-namespace UwpControlsDemo.Controls
+namespace UwpControlsDemo
 {
     public class EventListView : ListView
     {
-       
+        public EventListView()
+        {
+            _items = new Dictionary<object, DateTime>();
+        }
 
+        //protected override DependencyObject GetContainerForItemOverride()
+        //{
+        //    return new EventListViewItem();
+        //}
+
+
+        private Dictionary<object, DateTime> _items;
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            base.PrepareContainerForItemOverride(element, item);
+            return;
+            var eventListViewItem = element as EventListViewItem;
+            var time = _items[item];
+            eventListViewItem.Content = item;
+            eventListViewItem.Header = time.ToString("HH:mm:ss ffff");
+        }
+
+
+        protected override void OnItemsChanged(object e)
+        {
+            base.OnItemsChanged(e);
+            foreach (var item in Items)
+            {
+                if (_items.ContainsKey(item) == false)
+                    _items.Add(item, DateTime.Now);
+            }
+        }
     }
 }
---
+++ UwpControlsDemo/UwpControlsDemo/Controls/EventListViewItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace UwpControlsDemo
+{
+    public sealed class EventListViewItem : ContentControl
+    {
+        public EventListViewItem()
+        {
+            this.DefaultStyleKey = typeof(EventListViewItem);
+        }
+
+        public object Header
+        {
+            get { return (object)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Header.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(object), typeof(EventListViewItem), new PropertyMetadata(null));
+
+
+    }
+}
