f5643d21ba56f7b8af10cf22b73502e2de33f612
dino <dino.chan@live.com>
2017-01-30 23:32:48 +0800

    Add ListBoxPage

--- UwpControlsDemo/UwpControlsDemo/Samples/ComboBox/ComboBoxPage.xaml.cs
+++ UwpControlsDemo/UwpControlsDemo/Samples/ComboBox/ComboBoxPage.xaml.cs
@@ -25,6 +25,7 @@ namespace UwpControlsDemo
         public ComboBoxPage()
         {
             this.InitializeComponent();
+            
         }
     }
 }
--- UwpControlsDemo/UwpControlsDemo/Samples/ListBox/ListBoxPage.xaml.cs
+++ UwpControlsDemo/UwpControlsDemo/Samples/ListBox/ListBoxPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -20,11 +22,73 @@ namespace UwpControlsDemo
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class ListBoxPage : Page
+    public sealed partial class ListBoxPage : Page, INotifyPropertyChanged
     {
         public ListBoxPage()
         {
             this.InitializeComponent();
+            Actions = new ObservableCollection<string>();
+            SelectionModes = new List<SelectionMode> { SelectionMode.Extended, SelectionMode.Multiple, SelectionMode.Single };
+            Items = new ObservableCollection<int>();
+            for (int i = 0; i < 50; i++)
+            {
+                AddItem();
+            }
+        }
+
+
+        public ObservableCollection<string> Actions { get; }
+
+        public IEnumerable<SelectionMode> SelectionModes { get; }
+
+        public ObservableCollection<int> Items { get; private set; }
+
+        public bool CanRemoveItems
+        {
+            get
+            {
+                return _canRemoveItems;
+            }
+
+            set
+            {
+                if (_canRemoveItems == value)
+                    return;
+
+                _canRemoveItems = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(CanRemoveItems)));
+            }
+        }
+
+        private bool _canRemoveItems;
+
+        private int _index;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void AddAction(string action)
+        {
+            Actions.Add(action);
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            AddAction("SelectionChanged");
+            CanRemoveItems = ListBox.SelectedItems.Any();
+        }
+
+        private void AddItem()
+        {
+            Items.Add(_index++);
+        }
+
+        private void RemoveCurrentItems()
+        {
+            foreach (var item in ListBox.SelectedItems.ToList())
+            {
+                Items.Remove((int)item);
+            }
         }
     }
 }
