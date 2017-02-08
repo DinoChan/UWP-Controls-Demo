672bb810c98f85f24136c97e138c08018a3b3134
Dino.Chan <dino.chan@live.com>
2017-02-08 14:18:52 +0800

    Modify ProgressRingPage

--- UwpControlsDemo/UwpControlsDemo/Samples/ProgressBar/ProgressBarPage.xaml.cs
+++ UwpControlsDemo/UwpControlsDemo/Samples/ProgressBar/ProgressBarPage.xaml.cs
@@ -25,6 +25,7 @@ namespace UwpControlsDemo
         public ProgressBarPage()
         {
             this.InitializeComponent();
+            
         }
     }
 }
--- UwpControlsDemo/UwpControlsDemo/Samples/ProgressRing/ProgressRingPage.xaml.cs
+++ UwpControlsDemo/UwpControlsDemo/Samples/ProgressRing/ProgressRingPage.xaml.cs
@@ -25,6 +25,7 @@ namespace UwpControlsDemo
         public ProgressRingPage()
         {
             this.InitializeComponent();
+            
         }
     }
 }
