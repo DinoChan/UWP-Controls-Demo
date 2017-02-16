85adb779d1ff92263efbc5b98594206bc97c5829
Dino.Chan <dino.chan@live.com>
2017-02-16 11:17:37 +0800

    Change AutoSuggestBoxPage

--- UwpControlsDemo/UwpControlsDemo/Samples/AutoSuggestBox/AutoSuggestBoxPage.xaml.cs
+++ UwpControlsDemo/UwpControlsDemo/Samples/AutoSuggestBox/AutoSuggestBoxPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -21,17 +22,46 @@ namespace UwpControlsDemo
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class AutoSuggestBoxPage : Page
+    public sealed partial class AutoSuggestBoxPage : Page, INotifyPropertyChanged
     {
         public AutoSuggestBoxPage()
         {
             this.InitializeComponent();
             Actions = new ObservableCollection<string>();
+            var _itemsSourceCore = new ObservableCollection<string>();
+            for (int i = 0; i < 10000; i++)
+            {
+                _itemsSourceCore.Add(i.ToString());
+            }
+            UpdateItemsSource(string.Empty);
         }
 
 
         public ObservableCollection<string> Actions { get; }
 
+
+        private IEnumerable<string> _itemsSource;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 获取或设置 Property 的值
+        /// </summary>
+        public IEnumerable<string> ItemsSource
+        {
+            get { return _itemsSource; }
+            set
+            {
+                if (_itemsSource == value)
+                    return;
+
+                _itemsSource = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(ItemsSource)));
+            }
+        }
+
+        private ObservableCollection<string> _itemsSourceCore;
 
         private void AddAction(string action)
         {
@@ -41,17 +71,34 @@ namespace UwpControlsDemo
         private void OnQuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
         {
             AddAction("QuerySubmitted");
+            if (args.ChosenSuggestion != null)
+            {
+                // User selected an item from the suggestion list, take an action on it here.
+            }
+            else
+            {
+                // Use args.QueryText to determine what to do.
+            }
         }
-
 
         private void OnSuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
         {
             AddAction("SuggestionChosen");
+            sender.Text = (string)args.SelectedItem;
         }
 
         private void OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
             AddAction("TextChanged");
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                UpdateItemsSource(sender.Text);
+            }
+        }
+
+        private void UpdateItemsSource(string filter)
+        {
+            ItemsSource = _itemsSourceCore.Where(i => i.StartsWith(filter));
         }
     }
 }
